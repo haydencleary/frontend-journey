e89c7239d57d52aea5280b08ff7aec82d6b48696
haydencleary <5160414+haydencleary@users.noreply.github.com>
2022-06-18 22:43:19 +0200

    Added DM sans

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['DM sans', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
-	plugins: [
-		require('@tailwindcss/typography'),
-		require('@tailwindcss/aspect-ratio')
-	]
-}
+	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio')]
+};
