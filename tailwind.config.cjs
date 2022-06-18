fc396cd370afa31f41d0bdb29b59f03f3e9350e4
haydencleary <5160414+haydencleary@users.noreply.github.com>
2022-06-18 23:08:02 +0200

    Updated theme and spacing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,18 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			black: colors.black,
+			white: colors.white,
+			brand: colors.cyan,
+			gray: colors.slate
+		},
 		extend: {
 			fontFamily: {
 				sans: ['DM sans', ...defaultTheme.fontFamily.sans]
